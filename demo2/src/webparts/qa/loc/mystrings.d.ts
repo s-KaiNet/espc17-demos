ecb8a6956ca2e19eb3dfd7ef6b1bfe77e591fb6a
Sergei Sergeev <sergeev.srg@gmail.com>
2017-10-04 23:27:48 +0300

    Initial add.

---
+++ demo2/src/webparts/qa/loc/mystrings.d.ts
@@ -0,0 +1,10 @@
+declare interface IQAWebPartStrings {
+  PropertyPaneDescription: string;
+  BasicGroupName: string;
+  DescriptionFieldLabel: string;
+}
+
+declare module 'QAWebPartStrings' {
+  const strings: IQAWebPartStrings;
+  export = strings;
+}
